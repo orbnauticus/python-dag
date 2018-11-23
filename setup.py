66458b5109c298f33c8faea0855d1bf68b062303
Ryan Marquardt <ryan@integralws.com>
2018-11-23 18:15:36 +0000

    Initial packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='python-dag',
+    version='1.0',
+    description='Directed Acyclic Graph Implementation',
+    author='Ryan Marquardt',
+    author_email='ryan.marquardt@gmail.com',
+    url='https://github.com/ryanmarquardt/python-dag',
+    packages=['dag'],
+)
